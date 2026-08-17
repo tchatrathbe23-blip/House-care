31bddc405ca5e7a1c6f8dfb59b1b71703888e42d
tchatrathbe23-blip <tchatrath_be23@thapar.edu>
2026-08-17 18:37:15 +0530

    fix: update Mongoose async pre-save hooks for modern Mongoose compatibility

--- backend/models/User.js
+++ backend/models/User.js
@@ -39,16 +39,11 @@ const userSchema = new mongoose.Schema({
 });
 
 // Hash password before saving
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
   
-  try {
-    const salt = await bcryptjs.genSalt(10);
-    this.password = await bcryptjs.hash(this.password, salt);
-    next();
-  } catch (error) {
-    next(error);
-  }
+  const salt = await bcryptjs.genSalt(10);
+  this.password = await bcryptjs.hash(this.password, salt);
 });
 
 // Method to compare passwords
